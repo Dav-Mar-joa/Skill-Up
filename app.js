f559b695277e2a30e2cc038e3be2458ca2b26e6a
David MARTINS <david.joaquim.martins@gmail.com>
2025-06-28 03:30:38 +0200

    app use static

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ connectDB();
 // Configuration moteur Pug + dossier views et statics
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
